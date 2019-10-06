5d1e2aaab519b92c0475e522f1e359244aeae677
Rakesh Patil <patilrakesh1722@gmail.com>
2019-10-06 20:20:17 +0530

    Add files via upload

---
+++ scrapping.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+a = requests.get("https://coreyms.com").text
+b = BeautifulSoup(a,'lxml')
+
+csv_file = open('cms_scrap.csv','w')
+csv_writer= csv.writer(csv_file)
+csv_writer.writerow(['headline','summary','video_link'])
+
+for article in b.find_all('article'):
+    headline = article.h2.a
+    print(headline)
+
+    summary =article.find('div', class_='entry-content').p.text
+    print(summary)
+
+    try:
+        vid_src =article.find('iframe',class_='youtube-player')['src']
+        vid_src=vid_src.split('/')[4]
+        vid_src=vid_src.split('?')[0]
+        yt_link = f'https://youtube.com/watch?v={vid_src}'
+    except Exception as e:
+        yt_link = None
+    print(yt_link)
+    print()
+
+    
+    csv_writer.writerow([headline,summary,yt_link])
+csv_file.close()
+
+
